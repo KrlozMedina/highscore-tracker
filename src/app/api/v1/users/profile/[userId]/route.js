9aa9722cdf17ef61a87f36c21c8dad6b2443ac71
KrlozMedina <charli1216@hotmail.com>
2024-10-14 10:54:02 -0500

    Implementacion de la API

--- src/app/api/v1/users/profile/[userId]/route.js
+++ src/app/api/v1/users/profile/[userId]/route.js
@@ -5,4 +5,11 @@ import { NextResponse } from "next/server";
 import data from 'hst/mocks/users.json'
 
 export async function GET(req, res) {
-  const userId = res.pa
+  const userId = res.params.userId
+  return NextResponse.json(data)
+}
+
+export async function PUT(req, res) {
+  const userId = res.params.userId
+  return NextResponse.json(data[1], {status: 200})
+}
